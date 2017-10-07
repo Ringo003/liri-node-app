78aa4bf9ab1e01c00c060e13866ef322568695bc
John Reese <Johnreese1969@gmail.com>
2017-10-06 20:59:29 -0500

    update

---
+++ app.js
@@ -0,0 +1,13 @@
+var one = process.argv[2];
+var two = process.argv[3];
+
+if (one === two){
+	console.log(true);
+}else{
+	console.log(false);
+}
+if(( one % 7===0) &&)&& (two % 7 === 0)0 {
+	console.log(true);
+}else{
+	console.log(false);
+}
---
+++ keys.js
@@ -0,0 +1,17 @@
+var twitterKeys = {
+  consumer_key: '<8ZxV3hxoHatzstmV3wN1ZOufs>',
+  consumer_secret: '<4O7U0vz59JmCHpj77MFmWMlKjAfuWjbo1CYTu6BVJy3fuuOoqR>',
+  access_token_key: '<	913210221803294725-5OLKzLZef8LprCPzlYbCtxw2zg27CDt>',
+  access_token_secret: '<UUMUqlAZvHnjDnbdNHUMYx8GFVUOEXomCdKqdiaSs16JU>',
+}
+
+module.exports = {
+	twitterKeys: twitterKeys,
+	spotify: spotify
+
+};
+
+var spotify = {
+	client_ID: '<2f4bd4604d924717a5754de0ec6435be>'
+	client_secret'<964513f557db4432878c9548fa8f4a48>'
+}
